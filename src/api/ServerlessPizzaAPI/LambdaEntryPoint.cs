6ae88b7b34e8bdebf258d98b77844e2690d20616
JP Dillingham <jpdillingham@gmail.com>
2019-07-05 18:11:55 -0500

    stub API

---
+++ src/api/ServerlessPizzaAPI/LambdaEntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Amazon.Lambda.AspNetCoreServer;
+
+namespace ServerlessPizzaAPI
+{
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>()
+                .UseApiGateway();
+        }
+    }
+}
