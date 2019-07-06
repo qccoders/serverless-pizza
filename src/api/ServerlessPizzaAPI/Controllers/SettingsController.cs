d08463bc73afd4b8c85549bd88de93c221857080
JP Dillingham <jpdillingham@gmail.com>
2019-07-05 22:30:44 -0500

    add settings controller

---
+++ src/api/ServerlessPizzaAPI/Controllers/SettingsController.cs
@@ -0,0 +1,36 @@
+﻿namespace ServerlessPizzaAPI.Controllers
+{
+    using System.Threading.Tasks;
+    using Amazon.DynamoDBv2;
+    using Amazon.DynamoDBv2.DocumentModel;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        private IAmazonDynamoDB Client { get; }
+
+        public SettingsController(IAmazonDynamoDB amazonDynamoDBClient)
+        {
+            Client = amazonDynamoDBClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<object>> Get()
+        {
+            Table table = Table.LoadTable(Client, "serverless-pizza-settings");
+
+            var crustsTask = table.GetItemAsync("crusts");
+            var toppingsTask = table.GetItemAsync("toppings");
+
+            await Task.WhenAll(crustsTask, toppingsTask);
+
+            dynamic crusts = JsonConvert.DeserializeObject((await crustsTask).ToJson());
+            dynamic toppings = JsonConvert.DeserializeObject((await toppingsTask).ToJson());
+
+            return new { Crusts = crusts.value, Toppings = toppings.value };
+        }
+    }
+}
--- src/api/ServerlessPizzaAPI/Startup.cs
+++ src/api/ServerlessPizzaAPI/Startup.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace ServerlessPizzaAPI
+﻿namespace ServerlessPizzaAPI
 {
+    using Amazon;
+    using Amazon.DynamoDBv2;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -21,13 +17,13 @@ namespace ServerlessPizzaAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IAmazonDynamoDB, AmazonDynamoDBClient>((s) => new AmazonDynamoDBClient(RegionEndpoint.USEast1));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
