1ace906b5d05d600771cf300e0d1bb89076a9e21
JP Dillingham <jpdillingham@gmail.com>
2019-07-06 15:09:35 -0500

    add Order model

---
+++ src/api/ServerlessPizzaAPI/Models/Details.cs
@@ -0,0 +1,20 @@
+﻿namespace ServerlessPizzaAPI.Models
+{
+    using System.Collections.Generic;
+
+    public class Details
+    {
+        public Details(string size, string type, int cookTime, IEnumerable<string> toppings)
+        {
+            Size = size;
+            Type = type;
+            CookTime = cookTime;
+            Toppings = toppings;
+        }
+
+        public string Size { get; }
+        public string Type { get; }
+        public int CookTime { get; }
+        public IEnumerable<string> Toppings { get; }
+    }
+}
